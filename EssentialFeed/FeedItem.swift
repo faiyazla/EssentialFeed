1f4a19697b62e05f29fb231b2ca8ec7865b5244f
faiyaz <f.ahmed@trameter.com>
2024-05-08 05:42:37 +0545

    Create FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Faiyaz Ahmed on 08/05/2024.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
