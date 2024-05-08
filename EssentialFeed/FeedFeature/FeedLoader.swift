0c6bc077f367227cf43a232eaad1092c011b6cb9
faiyaz <f.ahmed@trameter.com>
2024-05-08 05:47:44 +0545

    Group feed feature files

---
+++ EssentialFeed/FeedFeature/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Faiyaz Ahmed on 08/05/2024.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult>) -> Void)
+}
--- EssentialFeed/FeedLoader.swift
+++
@@ -1,8 +0,0 @@
-//
-//  FeedLoader.swift
-//  EssentialFeed
-//
-//  Created by Faiyaz Ahmed on 08/05/2024.
-//
-
-import Foundation
